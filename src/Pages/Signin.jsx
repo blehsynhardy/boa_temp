fc3b3e960e0fa3a4d53db5753a8aa3645c980bcc
unknown <blessingawodele@gmail.com>
2025-03-15 20:31:13 +0100

    added footer

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from "../assets/images/BofA_rgb.png";
+import Connected from "../assets/images/mobile_llama.png";
+import Padlock from "../assets/images/Padlock.png";
 import "./Signin.css";
 
 const Signin = () => {
@@ -36,9 +38,69 @@ const Signin = () => {
 
       <section className="hero_section">
         <div className="login_header">
-            <h1>Log In to Online Banking</h1>
+          <h1>Log In to Online Banking</h1>
         </div>
       </section>
+
+      <section className="login_section">
+        <div className="login_container">
+          <div className="login_form">
+            <form>
+              <div className="input_container">
+                <label for="email">User Id</label>
+                <input type="email" id="email" name="email" />
+              </div>
+              <div className="input_container">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" />
+              </div>
+
+              <div className="input_container">
+                <small>
+                  <a href="#">forgot your password</a>
+                </small>
+              </div>
+              <div className="input_container">
+                <span></span>
+                <button type="submit">Log In</button>
+              </div>
+            </form>
+          </div>
+
+          <div className="stay_connected">
+            <h1>Stay connected with our app</h1>
+            <div className="inner_connected">
+              <img src={Connected} alt="stay connected" width="180px" />
+              <div>
+                <p>
+                  Secure, convenient <br /> banking anytime
+                </p>
+                <button className="get_app">Get the App</button>
+              </div>
+            </div>
+          </div>
+
+          <div className="forget_password">
+            <h1>Login help</h1>
+            <hr width="200px" />
+            <p>Forgot ID/Password?</p>
+            <p>Problem logging in?</p>
+
+            <h1>Not using Online Banking?</h1>
+            <hr width="200px" />
+            <p>Enroll now</p>
+            <p>Learn more about Online Banking</p>
+            <p>Service Agreement</p>
+          </div>
+        </div>
+      </section>
+
+      <footer>
+        <div className="footer_container">
+            <p>secure Area</p>
+            <p>Privacy | Security | your choice privacy choice</p>
+        </div>
+      </footer>
     </div>
   );
 };
